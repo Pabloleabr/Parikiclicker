9b24f4babe285e888b3557465d4eb3db4f24221c
Pabloleabr <pabolo333@gmail.com>
2021-10-14 17:26:22 +0200

    added score and update on click

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 
-const canvas = document.getElementById("clickZone");
+const Canvas = document.getElementById("clickZone");
+const Score = document.getElementById("score")
+const Game = {
+    pFuerza: 0,
+    clickPower: 1,
+}
 
-canvas.onclick  = () =>{
+Canvas.onclick  = () =>{
     console.log("funciona");
+    Game.pFuerza+= Game.clickPower;
+    Score.innerText=Game.pFuerza;
 }
