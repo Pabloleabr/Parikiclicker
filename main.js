c1f8fb0ff7eedb87063f4ee9bf4dbb9dad7b8c19
Pabloleabr <pabolo333@gmail.com>
2021-10-15 20:49:08 +0200

    anyadido guar, cambio color al comprar y subido velocidad del interval

--- main.js
+++ main.js
@@ -1,33 +1,37 @@
 
-const bMenu = document.getElementById("buttonMenu");
-const Canvas = document.getElementById("clickZone");
-const Score = document.getElementById("score");
+const bMenu = document.getElementById("buttonMenu");//donde estan dentro todos los botones que se crean
+const Canvas = document.getElementById("clickZone");//la zona donde se clickea
+const Score = document.getElementById("score");//el contenedor de la puntuacion
 const COSTMULTI=0.5;
-const Game = {
+
+let Game ={//donde se guardan los datos del juego
     pFuerza: 0,
     clickPower: 1,
     cost1kg : 20,
     costProtes : 50,
     numProtes : 0,
     protesPower : 0.25,
+} 
+if( localStorage.length != 0){//te carga los datos guardados
+    Game = JSON.parse(localStorage.getItem("Game"));
 }
 /*
 * Esta funcion toma un strings, un coste inicial y una funcion como parametros
 * la funcion es la que se ejecutaria en el onclick y decuelce el elemento
-* button construido. P
+* button construido. 
 */
 function createButton(upgradeName, cost, onclickFunction){
     let b = document.createElement("button");
     let sp = document.createElement("span");
     sp.innerText = cost;
-    b.innerText = `${upgradeName} \ncost: `;
+    b.innerText = upgradeName;
     b.appendChild(sp);
     b.onclick = onclickFunction;
     b.className = "upgradeButtons";
     return b
 }
 
-function setScore(){
+function setScore(){//funcion que te actualiza los pFuerza
     Score.innerText= "Puntos de Fuerza: " + Game.pFuerza.toFixed(0);
 }
 
@@ -36,33 +40,52 @@ Canvas.onclick  = () =>{
     setScore();
 }
 
+function insuficientePuntos(button, pagoEfectuado){//cambia el color del coste
+    pagoEfectuado ? button.childNodes[3].style.color = "green" : button.childNodes[3].style.color = "red";
+    //anyadir animacion al pulsar alomejor
+    setTimeout(() => {
+        button.childNodes[3].style.color = "black";
+    }, 1000);
+}
 /*
 esto seria un ejemplo de un boton basico que sube la fuerza 
 asigana el valor del coste en el objeto Game
 La variable CONSTMULTI sirve para incrementar el precio de la mejora
+(Tener en cuenta que se ha cambiado para que ne le nombre haya que poner 
+\ncost: para que se muestre el coste ya que asi tiene mas flexibilidad 
+a la hora de que boton crear)
 */
-buttonDeUnKg = createButton("+1kg", Game.cost1kg, function(){
-    if(Game.pFuerza>=Game.cost1kg){//si tienes su coste te deja comprarlo
+buttonDeUnKg = createButton("+1kg \ncost: ", Game.cost1kg, function(){
+    let comp = Game.pFuerza>=Game.cost1kg;
+    if(comp){//si tienes su coste te deja comprarlo
         Game.clickPower +=0.2;
         Game.pFuerza -=Game.cost1kg;
         Game.cost1kg += Game.cost1kg*COSTMULTI;
     }
+    insuficientePuntos(buttonDeUnKg,comp);
 });
+bMenu.appendChild(buttonDeUnKg);
 
-buttonDeProtes = createButton("Batido de proteínas", Game.costProtes, function(){
-    if(Game.pFuerza>=Game.costProtes){//si tienes su coste te deja comprarlo
+buttonDeProtes = createButton("Batido de proteínas \ncost: ", Game.costProtes, function(){
+    let comp = Game.pFuerza>=Game.costProtes;
+    if(comp){//si tienes su coste te deja comprarlo
         Game.numProtes++;
         Game.pFuerza -=Game.costProtes;
         Game.costProtes += Game.costProtes*COSTMULTI;
     }
+    insuficientePuntos(buttonDeProtes,comp);
 });
-bMenu.appendChild(buttonDeUnKg);
 bMenu.appendChild(buttonDeProtes);
 
-setInterval(() => {
+buttonGuardar = createButton("Guardar", "", function(){
+    localStorage.setItem("Game", JSON.stringify(Game));
+});
+bMenu.appendChild(buttonGuardar);
+
+setInterval(() => {//bucle que se llama cada 0.5 seg para actualizar los datos del navegador
     buttonDeUnKg.childNodes[3].innerText = (Game.cost1kg).toFixed(0);
     buttonDeProtes.childNodes[3].innerText = (Game.costProtes).toFixed(0);
     Game.pFuerza += Game.protesPower*Game.numProtes;
     setScore();
-},500);
+},100);
 
